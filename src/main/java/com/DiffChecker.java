9a01faf2b2e579409a89b7c76d88faac8ece0f39
vksychev <vksychev@yandex.ru>
2018-05-03 05:09:45 +0300

    sql changes

--- src/main/java/com/DiffChecker.java
+++ src/main/java/com/DiffChecker.java
@@ -7,9 +7,18 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class DiffChecker {
-  private static String SQL_STRING =
+  private static String SQL_STRING = "select\n" +
+                                      "MIN(table_name) as table_name,\n" +
+                                      "  inn_1,\n" +
+                                      "  kpp_1,\n" +
+                                      "  inn_2,\n" +
+                                      "  kpp_2,\n" +
+                                      "  money,\n" +
+                                      "  tax\n" +
+                                      " FROM\n" +
+                                      "(\n" +
                                       "  select\n" +
-                                      "'seller' as table_name\n" +
+                                      "'seller' as table_name,\n" +
                                       "  inn_1,\n" +
                                       "  kpp_1,\n" +
                                       "  inn_2,\n" +
@@ -19,15 +28,23 @@ public class DiffChecker {
                                       "  from default.seller\n" +
                                       "  union all\n" +
                                       "  select\n" +
-                                      "'customer' as table_name\n" +
+                                      "'customer' as table_name,\n" +
                                       "  inn_1,\n" +
                                       "  kpp_1,\n" +
                                       "  inn_2,\n" +
                                       "  kpp_2,\n" +
                                       "  money,\n" +
                                       "  tax\n" +
-                                      "  from default.customer\n" ;
-
+                                      "  from default.customer\n" +
+                                      ") tmp\n" +
+                                      "group by\n" +
+                                      "  inn_1,\n" +
+                                      "  kpp_1,\n" +
+                                      "  inn_2,\n" +
+                                      "  kpp_2,\n" +
+                                      "  money,\n" +
+                                      "  tax\n" +
+                                      "having count(*) = 1  \n";
 
   public static void DiffTableGenerate(SparkSession spark) {
     Dataset<Row> diffDF = spark.sql(SQL_STRING);
